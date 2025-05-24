83acc37e8f0d49faa64091d1129d9b94fa0c3654
Jyothi Thumpati <jthumpati@gmail.com>
2025-05-24 16:12:48 -0400

    Added Student Model

--- src/main/java/com/SMS/Project1/Repository/StudentRepository.java
+++ src/main/java/com/SMS/Project1/Repository/StudentRepository.java
@@ -1,4 +1,11 @@
 package com.SMS.Project1.Repository;
 
-public interface StudentRepository {
+import com.SMS.Project1.Model.Student;
+import jakarta.persistence.Id;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
+
 }
--- src/main/java/com/SMS/Project1/Services/StudentService.java
+++ src/main/java/com/SMS/Project1/Services/StudentService.java
@@ -1,4 +1,13 @@
 package com.SMS.Project1.Services;
 
+import com.SMS.Project1.Model.Student;
+import com.SMS.Project1.Repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class StudentService {
+    @Autowired
+    private StudentRepository studentRepository;
+
 }
