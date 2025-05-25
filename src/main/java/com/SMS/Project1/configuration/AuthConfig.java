7bda56b00c60017a2611c6d426e52e65fb1b7914
lokeshsamudrala <lokeshsamudrala7288@gmail.com>
2025-05-25 09:28:19 -0400

    Added Auth Code

--- src/main/java/com/SMS/Project1/configuration/AuthConfig.java
+++ src/main/java/com/SMS/Project1/configuration/AuthConfig.java
@@ -1,4 +1,34 @@
 package com.SMS.Project1.configuration;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebSecurity
 public class AuthConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/","/login", "/register").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin((form) -> form
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/dashboard",true)
+                        .permitAll()
+                )
+                .logout((logout) -> logout
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/login?logout")
+                        .permitAll());
+
+        return http.build();
+    }
+
 }
