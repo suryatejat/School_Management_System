1a17a8950dd35cabf4f6c1ed9bba7c63c16d9810
vamsisidda <vamsisidda777@gmail.com>
2025-05-25 13:19:34 -0400

    Added Courses Controller

--- src/main/java/com/SMS/Project1/Services/CourseService.java
+++ src/main/java/com/SMS/Project1/Services/CourseService.java
@@ -1,4 +1,39 @@
 package com.SMS.Project1.Services;
 
+import com.SMS.Project1.Model.Courses;
+import com.SMS.Project1.Repository.CourseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class CourseService {
+    @Autowired
+    private CourseRepository  repository;
+
+    public Courses save(Courses course) {
+        return repository.save(course);
+    }
+
+    public Optional<Courses> findById(Long id){
+        return repository.findById(id);
+
+    }
+
+    public List<Courses> getAllCourses() {
+        return repository.findAll();
+    }
+    public Optional<Courses> deleteCourses(long id ){
+        repository.deleteById(id);
+        return null;
+    }
+
+    public Optional<Courses> Updatedesc(Courses course) {
+        Optional<Courses> existingCourse = repository.findById(course.getId());
+        System.out.println(existingCourse);
+        return existingCourse;
+    }
+
 }
