d86f5f15bd56dd40cbfbe3d504909bc06acfbf65
Surya <suryat2020@gmail.com>
2025-05-25 15:23:43 -0400

    Modified Auth Service

--- src/main/java/com/SMS/Project1/Services/StudentService.java
+++ src/main/java/com/SMS/Project1/Services/StudentService.java
@@ -4,12 +4,16 @@ import com.SMS.Project1.Model.Courses;
 import com.SMS.Project1.Model.Student;
 import com.SMS.Project1.Repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-public class StudentService {
+public class StudentService implements UserDetailsService {
     @Autowired
     private StudentRepository studentRepository;
 
@@ -17,20 +21,31 @@ public class StudentService {
         return studentRepository.save(student);
     }
 
-    public Student getByStudentId(Long id){
-        return studentRepository.getReferenceById(id);
+    public Student getByStudentId(Long id) throws UsernameNotFoundException {
+        return studentRepository.findById(id).orElseThrow(() -> new UsernameNotFoundException("Student not found"));
     }
+
     public Student UpdateByStudentId(long id, String email){
         Student existingStudent = getByStudentId(id);
         existingStudent.setEmail(email);
-        studentRepository.save(existingStudent);
-        return existingStudent;
+        return studentRepository.save(existingStudent);
     }
     public List<Student> getAllStudents(){
         return studentRepository.findAll();
     }
+
     public List<Courses> registeredCourses(Long id){
         return studentRepository.findById(id).get().getCourseList();
     }
 
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Student student = studentRepository.findStudentByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return User.builder()
+                .username(student.getEmail())
+                .password(student.getPassword())
+                .build();
+    }
 }
