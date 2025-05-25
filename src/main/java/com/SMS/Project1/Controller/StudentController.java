402ebf8bab21e19c9ad9e6420bf6e1b6779784c7
Jyothi Thumpati <jthumpati@gmail.com>
2025-05-25 13:10:29 -0400

    Added Student Controller

--- src/main/java/com/SMS/Project1/Controller/StudentController.java
+++ src/main/java/com/SMS/Project1/Controller/StudentController.java
@@ -1,4 +1,43 @@
 package com.SMS.Project1.Controller;
 
+import com.SMS.Project1.Model.Student;
+import com.SMS.Project1.Services.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/student")
 public class StudentController {
+
+    @Autowired
+    private StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @PostMapping("/add")
+    public Student addStudent(@RequestBody Student newStudent){
+//        System.out.println(newStudent);
+        return studentService.addStudent(newStudent);
+    }
+
+    @GetMapping("/{id}")
+    public Student getByStudentId(Long Id){
+        return studentService.getByStudentId(Id);
+    }
+
+    @PutMapping("/updateStudent")
+    public Student updateStudent(@RequestBody Student student){
+        System.out.println(student.getId() + " " + student.getEmail());
+        return studentService.UpdateByStudentId(student.getId(), student.getEmail());
+    }
+
+    @GetMapping("/")
+    public List<Student> getAllStudents(){
+        return studentService.getAllStudents();
+    }
 }
--- src/main/java/com/SMS/Project1/Services/StudentService.java
+++ src/main/java/com/SMS/Project1/Services/StudentService.java
@@ -1,9 +1,12 @@
 package com.SMS.Project1.Services;
 
+import com.SMS.Project1.Model.Courses;
 import com.SMS.Project1.Model.Student;
 import com.SMS.Project1.Repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class StudentService {
@@ -17,11 +20,17 @@ public class StudentService {
     public Student getByStudentId(Long id){
         return studentRepository.getReferenceById(id);
     }
-    public Student UpdateByStudentId(Student student){
-        Student existingStudent = getByStudentId(student.getId());
-        existingStudent.setEmail(student.getEmail());
+    public Student UpdateByStudentId(long id, String email){
+        Student existingStudent = getByStudentId(id);
+        existingStudent.setEmail(email);
+        studentRepository.save(existingStudent);
         return existingStudent;
     }
-
+    public List<Student> getAllStudents(){
+        return studentRepository.findAll();
+    }
+    public List<Courses> registeredCourses(Long id){
+        return studentRepository.findById(id).get().getCourseList();
+    }
 
 }
